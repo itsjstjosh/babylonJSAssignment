91db52552e446afca9c7a36be6dfa9702c40e3ce
itsjstjosh <b00798403@studentmail.uws.ac.uk>
2024-12-05 23:13:23 +0000

    copied from the lab folders to this assignment folder.

---
+++ babylonProj/village/src/createRunScene.ts
@@ -0,0 +1,11 @@
+import {} from "@babylonjs/core";
+
+import { SceneData } from "./interfaces";
+
+export default function createRunScene(runScene: SceneData) {
+ 
+
+  runScene.scene.onAfterRenderObservable.add(() => {});
+}
+
+
